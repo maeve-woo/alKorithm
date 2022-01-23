9493b94b51c863cc92b810ff75f0408cc8e8f0e1
maeve <ducktyping@kakao.com>
2022-01-23 22:54:07 +0900

    Sort시 짧은 길이가 앞으로 정렬되는 아이디어 활용

--- src/main/java/problem/study/전화번호목록.java
+++ src/main/java/problem/study/전화번호목록.java
@@ -1,5 +1,7 @@
 package problem.study;
 
+import java.util.Arrays;
+
 public class 전화번호목록 {
 	public static void main(String[] args) {
 		solution(new String[]{"119", "97674223", "1195524421"});
@@ -7,41 +9,39 @@ public class 전화번호목록 {
 
 	/*
 정확성  테스트
-테스트 1 〉	통과 (0.12ms, 77.1MB)
-테스트 2 〉	통과 (0.02ms, 73.6MB)
-테스트 3 〉	통과 (0.06ms, 76.1MB)
-테스트 4 〉	통과 (0.06ms, 77MB)
-테스트 5 〉	통과 (0.06ms, 72.4MB)
-테스트 6 〉	통과 (0.07ms, 81MB)
-테스트 7 〉	통과 (0.06ms, 73.8MB)
-테스트 8 〉	통과 (0.02ms, 76.6MB)
-테스트 9 〉	통과 (0.02ms, 79.3MB)
-테스트 10 〉	통과 (0.08ms, 73.2MB)
-테스트 11 〉	통과 (0.11ms, 75.7MB)
-테스트 12 〉	통과 (0.06ms, 71MB)
-테스트 13 〉	통과 (0.06ms, 73.6MB)
-테스트 14 〉	통과 (13.73ms, 80.3MB)
-테스트 15 〉	통과 (19.45ms, 83.8MB)
-테스트 16 〉	통과 (19.30ms, 85.4MB)
-테스트 17 〉	통과 (26.21ms, 81.6MB)
-테스트 18 〉	통과 (41.50ms, 86.6MB)
-테스트 19 〉	통과 (34.17ms, 84.5MB)
-테스트 20 〉	통과 (64.10ms, 90MB)
+테스트 1 〉	통과 (0.36ms, 79.9MB)
+테스트 2 〉	통과 (0.19ms, 76.7MB)
+테스트 3 〉	통과 (0.27ms, 76.2MB)
+테스트 4 〉	통과 (0.19ms, 75.4MB)
+테스트 5 〉	통과 (0.24ms, 77.6MB)
+테스트 6 〉	통과 (0.24ms, 76.9MB)
+테스트 7 〉	통과 (0.23ms, 78.7MB)
+테스트 8 〉	통과 (0.23ms, 70.5MB)
+테스트 9 〉	통과 (0.22ms, 72.9MB)
+테스트 10 〉	통과 (0.24ms, 79.7MB)
+테스트 11 〉	통과 (0.22ms, 73.9MB)
+테스트 12 〉	통과 (0.18ms, 75MB)
+테스트 13 〉	통과 (0.20ms, 72.9MB)
+테스트 14 〉	통과 (2.20ms, 76.6MB)
+테스트 15 〉	통과 (2.12ms, 69.6MB)
+테스트 16 〉	통과 (2.69ms, 73.5MB)
+테스트 17 〉	통과 (2.68ms, 78.7MB)
+테스트 18 〉	통과 (3.65ms, 75.2MB)
+테스트 19 〉	통과 (4.61ms, 83.3MB)
+테스트 20 〉	통과 (4.60ms, 77.6MB)
+
 효율성  테스트
-테스트 1 〉	통과 (0.10ms, 57.5MB)
-테스트 2 〉	통과 (0.03ms, 59.4MB)
-테스트 3 〉	실패 (시간 초과)
-테스트 4 〉	실패 (시간 초과)
+테스트 1 〉	통과 (30.31ms, 72.3MB)
+테스트 2 〉	통과 (14.28ms, 56.6MB)
+테스트 3 〉	통과 (364.57ms, 98MB)
+테스트 4 〉	통과 (253.30ms, 95.8MB)
 	 */
 	public static boolean solution(String[] phone_book) {
 		int length = phone_book.length;
-		for (int i = 0; i < length; i++) {
-			for (int j = i + 1; j < length; j++) {
-				if (phone_book[i].startsWith(phone_book[j])) {
-					return false;
-				} else if (phone_book[j].startsWith(phone_book[i])) {
-					return false;
-				}
+		Arrays.sort(phone_book);
+		for (int j = 0; j < length - 1; j++) {
+			if (phone_book[j + 1].startsWith(phone_book[j])) {
+				return false;
 			}
 		}
 		return true;
